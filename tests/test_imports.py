1e75769e370c34271f69a8c79e17f45eb6a4d3d2
gwajnberg <gabriel.wajnberg@hotmail.com>
2025-08-20 09:30:05 -0600

    CI test imports fix

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,6 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 def test_imports():
-    import fill_ontology_fields
-    import create_ontology_dict
-    import create_dict_of_samples
-    import gene_mode
-    import feed_vmr_table
+    from fill_ontology_fields import fill_ontology_fields
+    from create_ontology_dict import create_ontology_dict
+    from create_dict_of_samples import create_dict_of_samples
+    from create_dict_of_samples_one import create_dict_of_samples_one
+    from gene_mode import parse, insert_data
+    from feed_vmr_table import feed_vmr_table
